10f45ac0a771848bbd73eee5f9ced37c05036b7b
Emily Oppenheimer <opemily@gmail.com>
2016-06-22 23:17:02 -0400

    fixed homework

--- week_1/homework/js/main.js
+++ week_1/homework/js/main.js
@@ -5,26 +5,42 @@ myModule.controller('MyController', function () {
 
     self.red = {
         description: 'I\'m Red',
-        description2: 'Red is a primary color'
+        description2: 'Red is a primary color',
+        bodystyle: 'red-body',
+        firstdiv: 'red-first-div',
+        seconddiv: 'red-second-div'
     };
 
     self.blue = {
         description: 'I\'m Blue',
-        description2: 'Blue is a primary color'
+        description2: 'Blue is a primary color',
+        bodystyle: 'blue-body',
+        firstdiv: 'blue-first-div',
+        seconddiv: 'blue-second-div'
+
     };
 
     self.yellow = {
         description: 'I\'m Yellow',
-        description2: 'Yellow is a primary color'
+        description2: 'Yellow is a primary color',
+        bodystyle: 'yellow-body',
+        firstdiv: 'yellow-first-div',
+        seconddiv: 'yellow-second-div'
     };
 
     self.green = {
         description: 'I\'m Green',
-        description2: 'Green is a secondary color'
+        description2: 'Green is a secondary color',
+        bodystyle: 'green-body',
+        firstdiv: 'green-first-div',
+        seconddiv: 'green-second-div'
     };
 
     self.purple = {
         description: 'I\'m Purple',
-        description2: 'Purple is a secondary color'
+        description2: 'Purple is a secondary color',
+        bodystyle: 'purple-body',
+        firstdiv: 'purple-first-div',
+        seconddiv: 'purple-second-div'
     };
 });
---
+++ week_2/untitled folder/js/main.js
@@ -0,0 +1,6 @@
+var myModule = angular.module('MyApp', []);
+
+myModule.controller('MyController', function () {
+  var self = this;
+  
+});
